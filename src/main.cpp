5b1621eaf5dd11db5060c15d4f1229930b2e0189
Terry Dervaux <terry.dervaux@wiseed.com>
2023-07-08 12:25:49 +0200

    feat: add relay activation

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,30 @@
 #include <Arduino.h>
 
 #define PIN_LED 16
+#define PIN_RELAY 5
 
 // put function declarations here:
 int blinkLED(int);
+bool activateRelay(bool);
 
 void setup() {
   // setup LED
   pinMode(PIN_LED, OUTPUT);
   digitalWrite(PIN_LED, LOW);
+
+  // setup Relay
+  pinMode(PIN_RELAY, OUTPUT);
+  digitalWrite(PIN_RELAY, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int result = blinkLED(2000);
+  
+  activateRelay(true);
+  delay(10000);
+  activateRelay(false);
+  delay(10000);
 }
 
 
@@ -27,4 +38,14 @@ int blinkLED(int delayValue) {
   }
 
   return 1;
+}
+
+bool activateRelay(bool activate) {
+  if(activate){
+    digitalWrite(PIN_RELAY, HIGH);
+    return true;
+  }
+  
+  digitalWrite(PIN_RELAY, LOW);
+  return false;
 }
